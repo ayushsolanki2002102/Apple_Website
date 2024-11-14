b1fe692558426951d5e5e5c895b424d798f343ea
Ayush Solanki <ayushsolanki2002102@gmail.com>
2024-11-14 13:20:34 +0530

    Model WIP

---
+++ src/components/ModelView.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, size, item}) => {
+  return (
+//    <View
+//    index={index}
+//    id={gsapType}
+//    className={`border-2 border-red-500 w-full h-full $ {index === 2} ? `right-[-100%]}
+//    >
+
+//    </View>
+<>
+</>
+  )
+}
+
+export default ModelView
