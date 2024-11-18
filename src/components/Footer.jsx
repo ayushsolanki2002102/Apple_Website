49e0528ef78f0a5ddc54cb7154d18764aea6d390
Ayush Solanki <ayushsolanki2002102@gmail.com>
2024-11-18 18:19:17 +0530

    Footer Implemented

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { footerLinks } from '../constants'
 
 const Footer = () => {
   return (
@@ -16,6 +17,27 @@ const Footer = () => {
                     </span>
                     near you.
                 </p>
+                <p className='font-semibold text-gray text-xs'>
+                    or call 000800-040-1966
+                    </p>
+            </div>
+
+            <div className='bg-neutral-700 my-5 h-[1px] w-full'/>
+
+            <div className='flex md:flex-row flex-col md:items-center justify-between'>
+                <p className='font-semibold text-gray text-xs'>Copyright @ 2024 Apple Inc. All rights reserved.</p>
+                <div className='flex'>
+                    {footerLinks.map((link,i) => (
+                        <p key={link} className='font semibold text-gray text-xs'>
+                            {link}{' '}
+                            {i !== footerLinks.length -1 && (
+                                <span className='mx-2'> | </span>
+                            )}
+                        </p>
+                    ))}
+
+                </div>
+
             </div>
 
         </div>
